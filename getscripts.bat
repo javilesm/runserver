0a83997893ebb7bae6af634ef25fb67157422aeb
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-14 22:16:38 -0600

    Update getscripts.bat

--- getscripts.bat
+++ getscripts.bat
@@ -1,17 +1,21 @@
 @echo off
 
-set server_host=ec2-3-220-58-75.compute-1.amazonaws.com
-set server_user=ubuntu
-set private_key_path=C:\Users\jorge\.ssh\samava.pem
+set HostName=ec2-3-220-58-75.compute-1.amazonaws.com
+set User=ubuntu
+set IdentityFile=C:\Users\jorge\.ssh\samava.pem
 set local_file_path=C:\Users\jorge\.ssh\getscripts.sh
-set server_file_path=/home/%server_user%/getscripts.sh
+set server_file_path=/home/%User%/getscripts.sh
 
+echo HostName: %HostName%
+echo User: %User%
 echo Ejecutando la copia del archivo al servidor...
-scp -v -i "%private_key_path%" "%local_file_path%" "%server_user%@%server_host%:%server_file_path%"
+scp -v -i "%IdentityFile%" "%local_file_path%" "%User%@%HostName%:%server_file_path%"
 
 if %errorlevel% equ 0 (
   echo Copia de archivo completada exitosamente.
- ) else (
+  echo Ejecutando comandos en el servidor...
+  ssh -i "%IdentityFile%" "%User%@%HostName%" "sudo sudo apt-get install dos2unix && sudo dos2unix /home/ubuntu/getscripts.sh && sudo mv /home/ubuntu/getscripts.sh /home/ubuntu/getscripts && sudo chown $USER:$USER /home/ubuntu/getscripts && sudo chmod +x /home/ubuntu/getscripts && sudo bash /home/ubuntu/getscripts"
+  echo Comandos en el servidor ejecutados exitosamente.
+) else (
   echo Error durante la copia de archivo al servidor.
 )
-
