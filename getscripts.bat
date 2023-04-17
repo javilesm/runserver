a194f7f6c00b532827205a4f6b0538675bec997f
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-17 15:28:52 -0600

    Update getscripts.bat

--- getscripts.bat
+++ getscripts.bat
@@ -1,36 +1,39 @@
 @echo off
-REM En este script, se utiliza la variable especial "%~dp0" para obtener la ruta del directorio donde se encuentra el script Batch. Luego, se utiliza esta ruta para construir las rutas de archivo de las variables "local_file_path" y "Config_Path" concatenando la ruta del directorio del script con los nombres de archivo "getscripts.sh" y "config" respectivamente. De esta manera, las rutas de archivo se obtendrán dinámicamente desde el directorio donde se encuentre el script, lo que permite que el script sea más portátil y se pueda ejecutar desde diferentes ubicaciones sin necesidad de modificar las rutas de archivo de manera estática.
 
+rem Definir las variables para directorios locales
 set script_dir=%~dp0
-set server_file_path=/home/%server_user%/getscripts.sh
-set local_file_path=%script_dir%\getscripts.sh
-set config_file=%script_dir%config
+set local_file_path=%script_dir%\getscripts.sh          
+set Config_Path=%script_dir%\config                      
 
-echo ************************
-echo ***GITHUB SYNC SCRIPT***
-echo ************************
-
-for /f "usebackq tokens=1,2 delims= " %%i in ("%config_file%") do (
-  if "%%i"=="HostName" set HostName=%%j
-  if "%%i"=="User" set User=%%j
-  if "%%i"=="IdentityFile" set IdentityFile=%%j
+rem Leer los valores de las variables 'HostName', 'User', 'IdentityFile' desde el archivo 'config'
+for /f "usebackq tokens=1,2 delims= " %%i in ("%Config_Path%") do (
+  if "%%i"=="HostName" set HostName=%%j                  
+  if "%%i"=="User" set User=%%j                          
+  if "%%i"=="IdentityFile" set IdentityFile=%%j          
 )
 
-echo HostName: !HostName!
-echo User: !User!
-echo IdentityFile: !IdentityFile!
+rem Eliminar espacios en blanco alrededor de los valores de las variables
+set HostName=%HostName: =%
+set User=%User: =%
+set IdentityFile=%IdentityFile: =%
+
+rem Mostrar los valores de las variables 'HostName', 'User'
+echo IdentityFile: %IdentityFile%                        
+echo HostName: %HostName%                                
+echo User: %User%                                        
+
+rem Defir las variables para directorios remotos    
+set server_file_path=/home/%User%/getscripts.sh   
+
+rem Copiar archivos locales a directorios remotos
 echo Ejecutando la copia del archivo al servidor...
-scp -v -i "%IdentityFile%" "%local_file_path%" "%User%@%HostName%:%server_file_path%"
+scp -v -i "%IdentityFile%" "%local_file_path%" "%User%@%HostName%:%server_file_path%"   
 
 if %errorlevel% equ 0 (
   echo Copia de archivo completada exitosamente.
   echo Ejecutando comandos en el servidor...
-  ssh -i "%IdentityFile%" "%User%@%HostName%" "sudo sudo apt-get install dos2unix && sudo dos2unix /home/ubuntu/getscripts.sh && sudo mv /home/ubuntu/getscripts.sh /home/ubuntu/getscripts && sudo chown $USER:$USER /home/ubuntu/getscripts && sudo chmod +x /home/ubuntu/getscripts && sudo bash /home/ubuntu/getscripts"
+  ssh -i "%IdentityFile%" "%User%@%HostName%" "sudo sudo apt-get install dos2unix && sudo dos2unix /home/%User%/getscripts.sh && sudo mv /home/%User%/getscripts.sh /home/%User%/getscripts && sudo chown $USER:$USER /home/%User%/getscripts && sudo chmod +x /home/%User%/getscripts && sudo bash /home/%User%/getscripts"
   echo Comandos en el servidor ejecutados exitosamente.
 ) else (
   echo Error durante la copia de archivo al servidor.
 )
-
-echo ************************
-echo **********END***********
-echo ************************
