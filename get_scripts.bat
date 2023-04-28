fb088a26b420ccece10dd061d32f3cd1b60db696
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-28 17:35:28 -0600

    Update get_scripts.bat

--- get_scripts.bat
+++ get_scripts.bat
@@ -4,14 +4,18 @@ rem Definir las variables para directorios locales
 set config_file=config
 set script_dir=%~dp0
 set script=get_scripts.sh
+set credentials=git_credentials.txt
 set local_file_path=%script_dir%%script%
+set credentials_path=%script_dir%%credentials%
 set Config_Path=%script_dir%%config_file%
 
 rem Imprimir los valores de las variables
-echo config_file: %config_file%
+cho config_file: %config_file%
 echo script_dir: %script_dir%
 echo script: %script%
 echo local_file_path: %local_file_path%
+echo credentials: %credentials%
+echo credentials_path: %credentials_path%
 echo Config_Path: %Config_Path%
                   
 rem Leer los valores de las variables 'HostName', 'User', 'IdentityFile' desde el archivo 'config'
@@ -38,10 +42,11 @@ echo server_file_path: %server_file_path%
 
 rem Copiar archivos locales a directorios remotos
 echo Copiando archivos locales a directorios remotosr...
-scp -v -i "%IdentityFile%" "%local_file_path%" "%User%@%HostName%:%server_file_path%"   
+scp -v -i "%IdentityFile%" "%local_file_path%" "%User%@%HostName%:%server_file_path%"
+scp -v -i "%IdentityFile%" "%credentials_path%" "%User%@%HostName%:%server_file_path%"  
 
 if %errorlevel% equ 0 (
-  echo Copia de archivo completada exitosamente.
+  echo Copia de archivos locales a directorios remotos completada exitosamente.
   echo Ejecutando comandos en el servidor...
   ssh -i "%IdentityFile%" "%User%@%HostName%" "sudo apt-get install dos2unix && sudo dos2unix /home/%User%/%script% && sudo chown $USER:$USER /home/%User%/%script% && sudo chmod +x /home/%User%/%script% && sudo bash /home/%User%/%script%"
   echo Comandos en el servidor ejecutados exitosamente.
