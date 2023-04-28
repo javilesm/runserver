706798976eb427291c6c39abbde759ab8cbca2ad
javilesm <68437746+javilesm@users.noreply.github.com>
2023-04-28 17:26:53 -0600

    Update get_scripts.sh

--- get_scripts.sh
+++ get_scripts.sh
@@ -10,20 +10,19 @@ SCRIPT_DIR="$CURRENT_PATH/$REPOSITORY" # Directorio final
 UTILITIES_PATH="$SCRIPT_DIR/utilities"
 # Vector de sub-scripts a ejecutar recursivamente
 scripts=(
-    #"update_system.sh"
-    #"add_repositories.sh"
-    #"tree_install.sh"
-    #"zip_install.sh"
-    #"AWS/aws_install.sh"
-    #"JQ/jq_install.sh"
-    #"NGINX/nginx_install.sh"
-    #"NodeJS/nodejs_install.sh"
-    #"PHP/php_install.sh"
-    #"Python/python_install.sh"
+    "update_system.sh"
+    "add_repositories.sh"
+    "tree_install.sh"
+    "zip_install.sh"
+    "AWS/aws_install.sh"
+    "JQ/jq_install.sh"
+    "NGINX/nginx_install.sh"
+    "NodeJS/nodejs_install.sh"
+    "PHP/php_install.sh"
+    "Python/python_install.sh"
     "MySQL/mysql_install.sh"
-    #"PostgreSQL/postgresql_install.sh"
-    #"NEXTCLOUD/nextcloud_install.sh"
-
+    "NEXTCLOUD/nextcloud_install.sh"
+    "PostgreSQL/postgresql_install.sh"
 )
 # Función para leer credenciales desde archivo de texto
 function read_credentials() {
@@ -82,28 +81,23 @@ function update_git () {
         exit 1
     fi
 }
-# Función para actualizar la propiedad del directorio de destino
+# Función para actualizar la propiedad del directorio de destino y su contenido
 function update_dir_ownership () {
-    echo "Actualizando la propiedad del directorio $SCRIPT_DIR..."
-    if ! chown -R "$USER:$USER" "$SCRIPT_DIR"; then
-        echo "No se pudo actualizar la propiedad del directorio $SCRIPT_DIR. Por favor, revise los permisos del directorio y vuelva a intentarlo."
+    echo "Actualizando la propiedad del directorio $SCRIPT_DIR y su contenido..."
+    if ! chown -R "$USER:$USER" "$SCRIPT_DIR"/*; then
+        echo "No se pudo actualizar la propiedad del directorio $SCRIPT_DIR y su contenido. Por favor, revise los permisos del directorio y vuelva a intentarlo."
     fi
-    echo "La propiedad del directorio $SCRIPT_DIR fue actualizada para $USER..."
-    
-     echo "Actualizando la propiedad del directorio $UTILITIES_PATH..."
-    if ! chown -R "$USER:$USER" "$UTILITIES_PATH"; then
-        echo "No se pudo actualizar la propiedad del directorio $UTILITIES_PATH. Por favor, revise los permisos del directorio y vuelva a intentarlo."
-    fi
-    echo "La propiedad del directorio $UTILITIES_PATH fue actualizada para $USER..."
+    echo "La propiedad del directorio $SCRIPT_DIR y su contenido fueron actualizados para $USER."
 }
-# Función para verificar si el paquete dos2unix está instalado
+
+# Función para verificar si el paquete dos2unix está instalado y corregir todo el contenido y subdirectorios de SCRIPT_DIR
 function check_dos2unix () {
     echo "Verificando si el paquete dos2unix está instalado..."
     if command -v dos2unix >/dev/null 2>&1; then
-        echo "dos2unix está instalado. Se procederá a la corrección de los archivos."
+        echo "dos2unix está instalado. Se procederá a la corrección de los archivos en $SCRIPT_DIR y sus subdirectorios."
         find "$SCRIPT_DIR" -type f -exec dos2unix {} +
     else
-        echo "dos2unix no está instalado. Instálalo para corregir los archivos."
+        echo "dos2unix no está instalado. Instálalo para corregir los archivos en $SCRIPT_DIR y sus subdirectorios."
         exit 1
     fi
 }
@@ -204,15 +198,15 @@ function clean_system() {
 # Función principal
 function get_scripts () {
     echo "**********GET & RUN SCRIPTS***********"
-    #read_credentials
-    #check_directory
-    #update_dir_ownership
-    #check_dos2unix
-    #assign_execution_permissions
-    #create_symlinks
-    #create_symlinks2
-    #update_terminal_session
-    #update_system
+    read_credentials
+    check_directory
+    update_dir_ownership
+    check_dos2unix
+    assign_execution_permissions
+    create_symlinks
+    create_symlinks2
+    update_terminal_session
+    update_system
     validate_scripts
     run_scripts
     upgrade_system
